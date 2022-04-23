e4f0928c4d12d6d4e8ad96dc3c17d6fa4521287d
Nepolian-Pratik <44637259+Nepolian-Pratik@users.noreply.github.com>
2022-04-23 15:35:01 +0545

    Added echo logic and removed default api classes

--- Bots/SimpleBot.cs
+++ Bots/SimpleBot.cs
@@ -5,16 +5,16 @@ using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.Bot;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
 
 namespace demo1.Bots{
     public class SimpleBot : IBot{
-        public async Task OnTurn(ITurnContext turnContext, CancellationToken cancellationToken){
-            await turnContext.SendActivityAsync("Hello World!");
-        }
-
-        public Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if (turnContext.Activity.Type == ActivityTypes.Message){
+                var replyText = $"You sent: {turnContext.Activity.Text}";
+                await turnContext.SendActivityAsync(replyText);
+            }
         }
     }
 }
